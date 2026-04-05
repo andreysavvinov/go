ef3d946211bbb715945c313d10f0dad2014009b1
a_savvinov <a.savvinov@g.nsu.ru>
2026-04-05 20:30:59 +0700

    cherry pick commit to cherry pick 2

--- task1/helloworld.go
+++ task1/helloworld.go
@@ -3,7 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var hello string
-    hello = "Hello world"
-    fmt.Println(hello)
+	var hello string = "Hello world"
+	fmt.Println(hello) // Hello world
+
+	hello = "Hello Go"
+	fmt.Println(hello) // Hello Go
+
+	fmt.Println(hello) // Go Go Go Ole Ole Ole
+
+	var numbers [5]int = [5]int{1,2,3,4,5}
+    fmt.Printf("type of  element: %T\n", numbers[0])     // 1
+    fmt.Println(numbers[4])     // 5
+    numbers[0] = 87
+    fmt.Println(numbers[0])     // 87
 }
