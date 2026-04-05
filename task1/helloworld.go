5c060cab0a4cf9cd23214fa86dd2dc91bb35cd32
a_savvinov <a.savvinov@g.nsu.ru>
2026-04-05 19:58:57 +0700

    some chenges to merge into feature/task2

--- task1/helloworld.go
+++ task1/helloworld.go
@@ -1,11 +1,14 @@
 package main
-/*
-    Первая программа 
-    на языке Go
-*/
 
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world!")
+	var hello string = "Hello world"
+	fmt.Println(hello) // Hello world
+
+	hello = "Hello Go"
+	fmt.Println(hello) // Hello Go
+
+	hello = "Go Go Go Ole Ole Ole"
+	fmt.Println(hello) // Go Go Go Ole Ole Ole
 }
