67d81b7ce07a60e6b3ae4fa43a0f49ef31b75428
a_savvinov <a.savvinov@g.nsu.ru>
2026-04-05 20:30:59 +0700

    commit to cherry pick 2

--- task1/helloworld.go
+++ task1/helloworld.go
@@ -12,7 +12,7 @@ func main() {
 	fmt.Println(hello) // Go Go Go Ole Ole Ole
 
 	var numbers [5]int = [5]int{1,2,3,4,5}
-    fmt.Println(numbers[0])     // 1
+    fmt.Printf("type of  element: %T\n", numbers[0])     // 1
     fmt.Println(numbers[4])     // 5
     numbers[0] = 87
     fmt.Println(numbers[0])     // 87
