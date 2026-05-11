6c2ebabb723251c5494b0555d6c033c6ea6c83c1
a_savvinov <a.savvinov@g.nsu.ru>
2026-05-12 02:33:40 +0700

    quick_sort added and tested

--- StL/main.go
+++ StL/main.go
@@ -1,9 +1,15 @@
-package main 
+package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/andreysavvinov/go/StL/quick_sort"
+	"github.com/andreysavvinov/go/StL/stack"
+)
 
 func main() {
-	var intStack Stack[int]
+	//stack testing
+	var intStack stack.Stack[int]
 
 	intStack.Push(10)
 	intStack.Push(20)
@@ -11,11 +17,29 @@ func main() {
 	value, _ := intStack.Pop()
 	fmt.Println(value)
 
-	var stringStack Stack[string]
+	var stringStack stack.Stack[string]
 
 	stringStack.Push("Go")
 	stringStack.Push("Stack")
 
 	top, _ := stringStack.Peek()
 	fmt.Println(top)
+
+	//quick_sort testing
+
+	numbers := []int{5, 1, 9, 2, 7, 3}
+
+	quick_sort.QuickSort(numbers, func(a, b int) bool {
+		return a < b
+	})
+
+	fmt.Println(numbers)
+
+	words := []string{"go", "cpp", "java", "python"}
+
+	quick_sort.QuickSort(words, func(a, b string) bool {
+		return a < b
+	})
+
+	fmt.Println(words)
 }
